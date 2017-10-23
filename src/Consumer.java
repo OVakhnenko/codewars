fdba7c469103c87cf782053681256a3e8ec91cbb
ovakhnenko <it505606707IT!>
2017-10-23 18:55:45 +0300

    ...

---
+++ src/Consumer.java
@@ -0,0 +1,20 @@
+public class Consumer implements Runnable {
+    private final SingleElementBuffer buffer;
+
+    public Consumer(SingleElementBuffer buffer) {
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                int elem = buffer.get();
+                System.out.println(elem + " consumed");
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + " stopped.");
+                return;
+            }
+        }
+    }
+}
